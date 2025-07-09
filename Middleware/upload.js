2de1f3c02987d8f709947756a8b6516a61fa98a3
shital4879 <patilshital9730@gmail.com>
2025-07-09 18:56:52 +0530

    asdrtf

--- Middleware/upload.js
+++ Middleware/upload.js
@@ -1,35 +1,11 @@
-// import multer from 'multer';
-// import path from 'path';
-// import fs from 'fs';
-
-// // Storage engine
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     const folder = 'uploads/banners';
-//     if (!fs.existsSync(folder)) {
-//       fs.mkdirSync(folder, { recursive: true });
-//     }
-//     cb(null, folder);
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now();
-//     cb(null, `banner-${uniqueSuffix}${path.extname(file.originalname)}`);
-//   },
-// });
-
-// const uploadBanner = multer({ storage }).single('image');
-// export default uploadBanner;
-
-
-
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
-// Storage engine using persistent Render disk
+// Storage engine
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const folder = '/mnt/data/uploads/banners'; // Use persistent volume
+    const folder = 'uploads/banners';
     if (!fs.existsSync(folder)) {
       fs.mkdirSync(folder, { recursive: true });
     }
