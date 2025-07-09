e2564e5424eb21b206aaadd70ebbd1edafb94b4b
shital4879 <patilshital9730@gmail.com>
2025-07-09 15:49:27 +0530

    tyrew

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+// app.use('/uploads', express.static('uploads'));
 
 
 app.use('/api/banner',BannerRouter)
