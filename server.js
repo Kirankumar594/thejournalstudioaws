445404b4b26fdeb001a6b21f6eeabb793070aa2d
shital4879 <patilshital9730@gmail.com>
2025-07-09 15:47:36 +0530

    aaaaa

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+// app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+app.use('/uploads', express.static('uploads'));
+
 
 app.use('/api/banner',BannerRouter)
 app.use('/api/Testimonial',TestimonialRouter)
