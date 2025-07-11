23dbdebff86157df5350d3aba15c4f90189f94a7
shital4879 <patilshital9730@gmail.com>
2025-07-11 11:50:23 +0530

    asdfcvgh

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")))
+app.use("/uploads", express.static(path.join(process.cwd(), "Uploads")))
 // app.use('/uploads', express.static('uploads'));
 
 
