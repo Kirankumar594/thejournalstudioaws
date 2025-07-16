a7b7ee4b69962c74f867bd739173a8a447859bc6
shital4879 <patilshital9730@gmail.com>
2025-07-16 12:56:41 +0530

    lkjhgf

--- Middleware/upload.js
+++ Middleware/upload.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 // Storage engine
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const folder = 'uploads';
+    const folder = 'uploads/banners';
     if (!fs.existsSync(folder)) {
       fs.mkdirSync(folder, { recursive: true });
     }
